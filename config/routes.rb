a10f04aa37f96831cfea4cf4df062edc582c0f81
rsidhu <rahul@live-area.com>
2015-06-04 20:45:01 -0700

    Added first part of authentication. Only the ability to create a user. Added bCrypt gem. Went to the bathroom.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root 'maps#index'
   resources :maps
 
+  get "users" => "users#index"
+  get "users/new" => "users#new" 
+  post "users" => "users#create"
+
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
