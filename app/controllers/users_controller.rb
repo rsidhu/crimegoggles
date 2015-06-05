8a588940839ccf722705acb5415c47dd0f1333b3
rsidhu <rahul@live-area.com>
2015-06-04 20:56:20 -0700

    Authentication completed. Login and Logout functionality added. Added login/logout links in app layout. Spent some time thinking about how I'm going to ruin Sam's day if he keeps whistling the Jurassic Park theme.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
 	def index
-		@users = User.all
-  	end
+	  @users = User.all
+	end
 
-  	def new
-    	@user = User.new
-  	end
+	def new
+  	@user = User.new
+	end
 
-  	def create
-    	@user = User.new(user_params)
-    	if @user.save
-    	 	redirect_to users_path
-    	else
-      		render :new
-    	end
+	def create
+  	@user = User.new(user_params)
+  	if @user.save
+      session[:user_id] = @user.id.to_s
+  	 	redirect_to maps_path
+  	else
+    		render :new
   	end
+	end
 
-  	private
+	private
 
-  	def user_params
-    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  	end
+	def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end
