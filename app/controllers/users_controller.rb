ed96ed29c8908eb28395e9da3b6398b85ae673bb
rsidhu <rahul@live-area.com>
2015-06-05 14:50:33 -0700

    Added login splash page. Added elsif statement that took away the login/logout nav bar on the login splash page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,23 @@ class UsersController < ApplicationController
 	end
 
 	def create
-  	@user = User.new(user_params)
-  	if @user.save
-      session[:user_id] = @user.id.to_s
-  	 	redirect_to maps_path
-  	else
-    		render :new
-  	end
+  		@user = User.new(user_params)
+	  	if @user.save
+	      session[:user_id] = @user.id.to_s
+	  	 	redirect_to maps_path
+	  	else
+	    	render :new
+	  	end
 	end
+
+	def landing
+		unless session[:user_id]
+			redirect_to login_path
+		else
+			render 'maps/index'
+		end
+	end
+
 
 	private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'maps#index'
+  root 'users#landing'
   resources :maps
 
   get "users" => "users#index"
